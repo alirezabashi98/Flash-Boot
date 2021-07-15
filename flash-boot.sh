6638397f9b28a79dca11f6ffeaef37b1600ebb92
alirezabashi98 <alirezabashi98@gmail.com>
2021-07-15 20:16:29 +0430

    end project v0.1

---
+++ flash-boot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Enter the path of the ios file : " filePath
+
+read -p "Enter the arrow path You can use the lsblk command : " arrows
+
+echo $filePath
+echo $arrows
+
+sudo dd if=$filePath of=$arrows bs=4M status=progress
