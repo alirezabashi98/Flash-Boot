cca270a8b8702fa013cee62b87572b4bfc8fedcc
alirezabashi98 <alirezabashi98@gmail.com>
2021-07-16 07:42:36 +0430

    rm echo

--- flash-boot.sh
+++ flash-boot.sh
@@ -4,7 +4,4 @@ read -p "Enter the path of the ios file : " filePath
 
 read -p "Enter the arrow path You can use the lsblk command : " arrows
 
-echo $filePath
-echo $arrows
-
 sudo dd if=$filePath of=$arrows bs=3M status=progress
