045056ef05a3a82f697c7dcd026dcb982febb9a4
alirezabashi98 <alirezabashi98@gmail.com>
2021-07-16 07:17:28 +0430

    Change the procedure speed to 3M

--- flash-boot.sh
+++ flash-boot.sh
@@ -7,4 +7,4 @@ read -p "Enter the arrow path You can use the lsblk command : " arrows
 echo $filePath
 echo $arrows
 
-sudo dd if=$filePath of=$arrows bs=4M status=progress
+sudo dd if=$filePath of=$arrows bs=3M status=progress
